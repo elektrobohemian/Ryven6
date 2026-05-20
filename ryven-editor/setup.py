0424dbb23d9f5d9fe1d26c5f7813ea6e033353df
David Zellhöfer <david@8bit-inferno.de>
2026-05-20 17:10:05 +0200

    macOS installation update 2026
    * removed PySide2 dependency
    * removed PySide6 6.7 version constraint
    * added requirements.txt
    * modified standard configuration to include 'qt-api: pyside6'

--- ryven-editor/setup.py
+++ ryven-editor/setup.py
@@ -4,16 +4,19 @@ setup()
 
 # create ~/.ryven/ directory
 import os
-path = os.path.join(os.path.expanduser('~'), '.ryven/')  # == ryven_dir_path() in ryven.main.utils
+
+path = os.path.join(
+    os.path.expanduser("~"), ".ryven/"
+)  # == ryven_dir_path() in ryven.main.utils
 if not os.path.exists(path):
     os.mkdir(path)
-    os.mkdir(os.path.join(path, 'nodes/'))
-    os.mkdir(os.path.join(path, 'saves/'))
+    os.mkdir(os.path.join(path, "nodes/"))
+    os.mkdir(os.path.join(path, "saves/"))
 
 # generate default config files if not existent yet
-cfg_path = os.path.join(path, 'ryven.cfg')
+cfg_path = os.path.join(path, "ryven.cfg")
 if not os.path.exists(cfg_path):
-    with open(cfg_path, 'w') as f:
+    with open(cfg_path, "w") as f:
         f.write("""
 # Automatically loaded default Ryven config file
 
@@ -23,4 +26,5 @@ if not os.path.exists(cfg_path):
 #     flow-theme: "colorful light"
 #     nodes: std
 #     nodes: linalg
+qt-api: pyside6
 """)
